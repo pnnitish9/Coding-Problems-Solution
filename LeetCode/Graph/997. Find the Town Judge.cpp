0b551698c8a2ce84c69d4ab22603f89062bb6f8d
Nitish Kr <pnnitish88725@gmail.com>
2026-06-23 00:36:53 +0530

    town judge

---
+++ LeetCode/Graph/997. Find the Town Judge.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n+1, 0);
+        vector<int> outdegree(n+1, 0);
+
+        for(auto& it : trust){
+            int u = it[0];
+            int v = it[1];
+
+            outdegree[u]++;
+            indegree[v]++;
+        }
+
+        for(int i=1; i<=n; i++){
+            // cout<<i<<"--"<<indegree[i]<<" "<<outdegree[i]<<endl;
+            if(indegree[i] == n - 1 && outdegree[i]==0) return i;
+        }
+
+        return -1;
+    }
+};
