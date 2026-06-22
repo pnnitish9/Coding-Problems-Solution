982bd51fb4dfbaa76eea26609fdf13ab99e96944
Nitish Kr <pnnitish88725@gmail.com>
2026-06-23 00:08:18 +0530

    no. of provinces

---
+++ LeetCode/Graph/547. Number of Provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited){
+        visited[node] = 1;
+
+        for(auto it : adj[node]){
+            if(!visited[it]){
+                dfs(it, adj, visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adj(n+1);
+        vector<int> visited(n, 0);
+        for(int i=0; i<isConnected.size(); i++){
+            for(int j=0; j<isConnected[0].size(); j++){
+                if(isConnected[i][j] == 1)
+                    adj[i].push_back(j);
+            }
+        }
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                cnt++;
+                dfs(i, adj, visited);
+            }
+        }
+
+        return cnt;
+    }
+};
