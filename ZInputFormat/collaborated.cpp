5fb401490f3fe1c533b0b40c3690f54d1c8c31f8
Nitish Kr <pnnitish88725@gmail.com>
2026-06-27 17:19:47 +0530

    graph

--- ZInputFormat/collaborated.cpp
+++ ZInputFormat/collaborated.cpp
@@ -12,12 +12,32 @@ int main() {
     ss>>temp; ss>>temp; ss>>temp;
     ss>>c;
     
-    // (1-2),(2-3)
+    // (1-2),(2-3), (1-3)
     vector<vector<int>> adj(n+1);
     int t = c;
-    while(t--){
+    string edge; getline(cin, edge);
+    for(char &c : edge){
+        if(!isdigit(c)) c = ' ';
+    } 
 
+    stringstream ss2(edge);
+    int u, v;
+    while(ss2 >> u >> v ){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    cout<<n<<" "<<c<<endl;
+    
+
+    // cout<<n<<" "<<c<<endl;
+    int id = INT_MAX; 
+    int count = INT_MIN;
+    for(int i=1; i<=n; i++){
+        int size = adj[i].size();
+        if(count < size){
+            id = i;
+            count = size;
+        }
+    }
+    cout<<id<<" "<<count<<endl;
     return 0;
 }
